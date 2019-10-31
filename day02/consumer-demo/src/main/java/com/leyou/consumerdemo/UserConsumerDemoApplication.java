ceacd4608c51212fdce09ca7529722ec2c2da8ac
tiancixiong <tiancixiong@163.com>
2019-10-31 11:37:33 +0800

    Eureka入门案例

--- day02/consumer-demo/src/main/java/com/leyou/consumerdemo/UserConsumerDemoApplication.java
+++ day02/consumer-demo/src/main/java/com/leyou/consumerdemo/UserConsumerDemoApplication.java
@@ -2,11 +2,13 @@ package com.leyou.consumerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient // 开启Eureka客户端
 public class UserConsumerDemoApplication {
     @Bean
     public RestTemplate restTemplate() {
--- day02/consumer-demo/src/main/java/com/leyou/consumerdemo/service/impl/UserServiceImpl.java
+++ day02/consumer-demo/src/main/java/com/leyou/consumerdemo/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.leyou.consumerdemo.service.impl;
 
-import com.leyou.consumerdemo.mapper.UserDao;
 import com.leyou.consumerdemo.pojo.User;
 import com.leyou.consumerdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,15 +19,32 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    private UserDao userDao;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;// Eureka客户端，可以获取到服务实例信息
 
     @Override
-    public List<User> queryUserByIds(List<Long> ids){
+    public List<User> queryUserByIds(List<Long> ids) {
         List<User> users = new ArrayList<>();
-        for (Long id : ids) {
-            User user = this.userDao.queryUserById(id);
-            users.add(user);
-        }
+        // String baseUrl = "http://localhost:8081/user/";
+
+        // 根据服务名称，获取服务实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        // 因为只有一个UserService，因此我们直接get(0)获取
+        ServiceInstance instance = instances.get(0);
+        // 获取ip和端口信息
+        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/";
+        ids.forEach(id -> {
+            // 我们测试多次查询
+            users.add(this.restTemplate.getForObject(baseUrl + id, User.class));
+            // 每次间隔500ms
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         return users;
     }
 }
