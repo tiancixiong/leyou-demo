cf1b061f0940e1915e1e2f3268b9ccaf4137a1cc
tiancixiong <tiancixiong@163.com>
2019-10-31 13:24:28 +0800

    Ribbon入门案例

--- day02/consumer-demo/src/main/java/com/leyou/consumerdemo/UserConsumerDemoApplication.java
+++ day02/consumer-demo/src/main/java/com/leyou/consumerdemo/UserConsumerDemoApplication.java
@@ -3,6 +3,7 @@ package com.leyou.consumerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,10 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient // 开启Eureka客户端
 public class UserConsumerDemoApplication {
+
     @Bean
+    // 开启Ribbon负载均衡
+    @LoadBalanced
     public RestTemplate restTemplate() {
         // 这次我们使用了OkHttp客户端,只需要注入工厂即可
         return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
--- day02/consumer-demo/src/main/java/com/leyou/consumerdemo/service/impl/UserServiceImpl.java
+++ day02/consumer-demo/src/main/java/com/leyou/consumerdemo/service/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.leyou.consumerdemo.service.impl;
 import com.leyou.consumerdemo.pojo.User;
 import com.leyou.consumerdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -24,17 +23,39 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private DiscoveryClient discoveryClient;// Eureka客户端，可以获取到服务实例信息
 
+    // Eureka入门案例
+//    @Override
+//    public List<User> queryUserByIds(List<Long> ids) {
+//        List<User> users = new ArrayList<>();
+//        // String baseUrl = "http://localhost:8081/user/";
+//
+//        // 根据服务名称，获取服务实例
+//        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+//        // 因为只有一个UserService，因此我们直接get(0)获取
+//        ServiceInstance instance = instances.get(0);
+//        // 获取ip和端口信息
+//        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/";
+//        ids.forEach(id -> {
+//            // 我们测试多次查询
+//            users.add(this.restTemplate.getForObject(baseUrl + id, User.class));
+//            // 每次间隔500ms
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        return users;
+//    }
+
+    // Robbin学习
     @Override
     public List<User> queryUserByIds(List<Long> ids) {
         List<User> users = new ArrayList<>();
-        // String baseUrl = "http://localhost:8081/user/";
 
-        // 根据服务名称，获取服务实例
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
-        // 因为只有一个UserService，因此我们直接get(0)获取
-        ServiceInstance instance = instances.get(0);
-        // 获取ip和端口信息
-        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/";
+        // 地址直接写服务名称即可，使用Robbin负载均衡来调用user-service
+        String baseUrl = "http://user-service/user/";
+
         ids.forEach(id -> {
             // 我们测试多次查询
             users.add(this.restTemplate.getForObject(baseUrl + id, User.class));
